21c977d6f1fbf668995c9ee942bf6d3ed454dbb2
Marcos Yadiel <marcosyadiel@pm.me>
2026-06-15 14:58:10 -0400

    v6.0-D

--- src/config/appMeta.ts
+++ src/config/appMeta.ts
@@ -1,7 +1,7 @@
 export const APP_META = {
   name: 'LunaDash',
   version: '6.0',
-  build: 'prod.260615-B',
+  build: 'prod.260615-D',
   copyright: '© 2026 Glz Technical Services | Glz Tech',
   supportEmail: 'service@glztech.com',
   updateNotes: [
